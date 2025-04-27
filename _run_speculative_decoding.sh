14b7bd9cc934e97acf7576431009710c09276853
Gabriel Guo <gabeguo@atlas25.stanford.edu>
2025-04-27 13:31:51 -0700

    script to ruun full comparison between n-gram, parallel draft. also turned off assert statements

--- _run_speculative_decoding.sh
+++ _run_speculative_decoding.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --partition=atlas
 #SBATCH --account=atlas
-#SBATCH --job-name=dummy_speculative_decoding
+#SBATCH --job-name=speculative_decoding
 #
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1                # Single node
@@ -11,19 +11,17 @@
 #SBATCH --ntasks=1             # Number of tasks (one per GPU)
 
 # TODO: change output_dir and --finetuned_model_dir
-output_dir="/atlas/u/gabeguo/dummy_runs/speculative_decoding"
+output_dir="/atlas/u/gabeguo/neurips_2025/speculative_decoding/_full_comparison"
 
-python run_decoding_eval.py \
+python -O run_decoding_eval.py \
     --finetuned_model_dir therealgabeguo/ASARM \
     --start_percentage 0.05 \
-    --num_trials 100 \
+    --num_trials 700 \
     --eps 0 \
     --k 5 \
     --T 1 \
-    --output_dir $output_dir \
-    --skip_off_the_shelf
+    --output_dir $output_dir
 python eval_perplexity.py \
     --perplexity_model "gpt2-large" \
     --batch_size 4 \
-    --results_dir $output_dir \
-    --skip_off_the_shelf
+    --results_dir $output_dir
