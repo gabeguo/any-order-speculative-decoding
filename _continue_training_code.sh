4a39a8113abeff83d51f74d5462cd9220851c01b
Gabriel Guo <gabeguo@login14.chn.perlmutter.nersc.gov>
2025-04-28 11:01:02 -0700

    try training code model with 15% masking ratio

--- _continue_training_code.sh
+++ _continue_training_code.sh
@@ -3,7 +3,7 @@
 #SBATCH --reservation=asarm_scale
 #SBATCH --partition=m1266
 #SBATCH --account=m1266
-#SBATCH --job-name=code_training
+#SBATCH --job-name=code_single_line
 #SBATCH --constraint=gpu
 #SBATCH --qos=regular
 #SBATCH --time=144:00:00
@@ -17,25 +17,27 @@
 # select "openwebtext" or "bigcode/starcoderdata" dataset
 # change batch_size as desired
 
+export HF_HOME=$SCRATCH/cache/huggingface
+echo $HF_HOME
+
 #dataset="openwebtext"
 dataset="bigcode/starcoderdata"
 
 python -m torch.distributed.launch \
     --nproc_per_node=1 finetune_xlnet_distributed.py \
-    --batch_size 24 --accumulation_steps 4 \
+    --batch_size 24 --accumulation_steps 2 \
     --learning_rate 1e-5 \
     --warmup_steps 2500 --masking_warmup_steps 2500 \
-    --start_masking_rate 0.85 --final_min_masking_rate 0.85 --final_max_masking_rate 0.99 \
+    --start_masking_rate 0.01 --final_min_masking_rate 0.05 --final_max_masking_rate 0.25 \
     --items_per_epoch $((120*1000*1000)) --num_epochs 1 \
     --perm_mask_type joint \
-    --output_dir $PSCRATCH/xlnet_code \
-    --cache_dir $PSCRATCH/cache_sub \
+    --output_dir $PSCRATCH/xlnet_code_single_line \
+    --cache_dir $HF_HOME \
     --loss_scale_type scale_by_none \
     --eval_steps 5000 \
     --save_steps 2500 \
-    --eval_masking_rate 0.95 \
+    --eval_masking_rate 0.1 \
     --eval_num_decodes 1 \
     --wandb_project "finetune_xlnet_comparisons" \
     --packed_dataset \
-    --dataset $dataset \
-    --use_hf_model
+    --dataset $dataset
