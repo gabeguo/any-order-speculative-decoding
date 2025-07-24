8afc9cf2bf2322c0bcf49072e4657d7a9ec683e7
Gabriel Guo <gabeguo@sc.stanford.edu>
2025-07-24 14:09:17 -0700

    script to run ablations

---
+++ _run_ablations.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#SBATCH --partition=atlas
+#SBATCH --account=atlas
+#SBATCH --job-name=spec_decoding
+#
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1                # Single node
+#SBATCH --gpus=a4000:1
+#SBATCH --cpus-per-task=8       # CPUs for the job
+#SBATCH --ntasks=1             # Number of tasks (one per GPU)
+
+# TODO: change output_dir and --finetuned_model_dir
+for k in 2 3 5 10 15 20; do
+    for max_length in 128 256 512 1024; do
+        output_dir="/atlas/u/gabeguo/neurips_2025/speculative_decoding/k_${k}_max_length_${max_length}"
+
+        python -O run_decoding_eval.py \
+            --finetuned_model_dir therealgabeguo/ASARM \
+            --start_percentage 0.05 \
+            --num_trials 500 \
+            --eps 0 \
+            --k $k \
+            --max_length $max_length \
+            --T 1 \
+            --output_dir $output_dir \
+            --hf_revision nlp_210k_plus_75k \
+            --skip_off_the_shelf \
+            --skip_ngram
+        python eval_perplexity.py \
+            --perplexity_model "gpt2-large" \
+            --batch_size 4 \
+            --results_dir $output_dir \
+            --skip_off_the_shelf \
+            --skip_ngram
+    done
+done
