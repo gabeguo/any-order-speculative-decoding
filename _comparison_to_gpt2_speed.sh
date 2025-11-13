1d8eaa15152b062d091f075effea88f92f5b9c70
Gabriel Guo <gabeguo@scdt.stanford.edu>
2025-11-12 17:30:09 -0800

    bash script for gpt2 speed comparison

---
+++ _comparison_to_gpt2_speed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --partition=atlas
+#SBATCH --account=atlas
+#SBATCH --job-name=gpt_comparison
+#
+#SBATCH --time=120:00:00
+#SBATCH --nodes=1                # Single node
+#SBATCH --gpus=a4000:1
+#SBATCH --cpus-per-task=8       # CPUs for the job
+#SBATCH --ntasks=1            # Number of tasks (one per GPU)
+
+python eval_gpt2.py \
+    --n_trials 50 \
+    --seq_length 512 \
+    --prompt_percent 0.05 \
+    --output_dir /atlas/u/gabeguo/iclr2026_rebuttal/speed_comparison/gpt2
