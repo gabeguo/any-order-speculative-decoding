99ffc74baeb860de013c21b1750ef63165b08b7d
Gabriel Guo <gabeguo@atlas23.stanford.edu>
2025-11-16 14:22:56 -0800

    match length to 570, as this is approximately the ratio we need to get 512 tokens under gpt-2 tokenizer

--- _comparison_speed_alt_prompting.sh
+++ _comparison_speed_alt_prompting.sh
@@ -11,10 +11,11 @@
 #SBATCH --ntasks=1             # Number of tasks (one per GPU)
 
 # TODO: change output_dir and --finetuned_model_dir
-output_dir="/atlas/u/gabeguo/iclr2026_rebuttal/speed_comparison_redo/asarm_left_to_right"
+output_dir="/atlas/u/gabeguo/iclr2026_rebuttal/speed_comparison_redo/asarm_left_to_right_match_length"
 
 python -O run_decoding_eval.py \
     --finetuned_model_dir therealgabeguo/ASARM \
+    --max_length 570 \
     --start_percentage 0.05 \
     --num_trials 200 \
     --eps 0 \
